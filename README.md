af8c2e7f8721068eb8e9eba53a81e52b7a5f4a03
Nils <n.hehner@shopmacher.de>
2019-03-28 14:41:43 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,4 @@
+KontoVerwaltung
+===============
+
+A Symfony project created on March 21, 2019, 2:05 pm.
---
+++ web/build/app.js
@@ -0,0 +1,29 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["app"],{
+
+/***/ "./assets/css/app.css":
+/*!****************************!*\
+  !*** ./assets/css/app.css ***!
+  \****************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "./assets/js/app.js":
+/*!**************************!*\
+  !*** ./assets/js/app.js ***!
+  \**************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// assets/js/app.js
+__webpack_require__(/*! ../css/app.css */ "./assets/css/app.css");
+
+console.log('Hello Webpack Encore');
+
+/***/ })
+
+},[["./assets/js/app.js","runtime"]]]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hc3NldHMvY3NzL2FwcC5jc3MiLCJ3ZWJwYWNrOi8vLy4vYXNzZXRzL2pzL2FwcC5qcyJdLCJuYW1lcyI6WyJyZXF1aXJlIiwiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7O0FBQUEsdUM7Ozs7Ozs7Ozs7O0FDQUE7QUFFQUEsbUJBQU8sQ0FBQyw0Q0FBRCxDQUFQOztBQUVBQyxPQUFPLENBQUNDLEdBQVIsQ0FBWSxzQkFBWixFIiwiZmlsZSI6ImFwcC5qcyIsInNvdXJjZXNDb250ZW50IjpbIi8vIGV4dHJhY3RlZCBieSBtaW5pLWNzcy1leHRyYWN0LXBsdWdpbiIsIi8vIGFzc2V0cy9qcy9hcHAuanNcclxuXHJcbnJlcXVpcmUoJy4uL2Nzcy9hcHAuY3NzJyk7XHJcblxyXG5jb25zb2xlLmxvZygnSGVsbG8gV2VicGFjayBFbmNvcmUnKTsiXSwic291cmNlUm9vdCI6IiJ9

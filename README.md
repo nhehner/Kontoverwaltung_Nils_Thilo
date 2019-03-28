0f8b5a19e1a125bfe7c9eac093dac8dc2e56709a
Nils <n.hehner@shopmacher.de>
2019-03-28 16:41:46 +0100

    added vueJS & bootstrap

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 KontoVerwaltung
 ===============
 
-A Symfony project created on March 21, 2019, 2:05 pm. Test
+A Symfony project created on March 21, 2019, 2:05 pm. 
--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,8 @@ Encore
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
     .enableSingleRuntimeChunk()
+    .enableVueLoader()
+    .enableSassLoader()
 
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())

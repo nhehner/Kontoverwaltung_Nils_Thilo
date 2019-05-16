b8329533ab38f280248915ffa3be8690bd953a55
Nils <n.hehner@shopmacher.de>
2019-05-16 17:09:15 +0200

    some konto changes

--- src/AppBundle/Controller/KontoController.php
+++ src/AppBundle/Controller/KontoController.php
@@ -45,7 +45,54 @@ class KontoController extends FOSRestController
                             'bic' => $konto->getBic(),
                             'inhaber' => $konto->getInhaber(),
                             'kreditkarte' => $konto->getKreditkarte(),
-                            'gueltigBis' => $konto->getGueltigBis(),
+                            'gueltigBis' => $konto->getGueltigBis()->format('m/Y'),
+                            'aktiv' => $konto->getAktiv()
+                        ];
+                    }
+                }
+                return new View($result, Response::HTTP_OK);
+            } else {
+                return new View("Recieved non valid json data", Response::HTTP_BAD_REQUEST);
+            }
+        }
+        return new View("Recieved non valid json data", Response::HTTP_BAD_REQUEST);
+    }
+
+    /**
+     * @Rest\Post("/createKonto")
+     * @return View
+     */
+    public function createKonto()
+    {
+        $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
+
+        if ($contentType === "application/json") {
+            $content = trim(file_get_contents("php://input"));
+            $decoded = json_decode($content, true);
+
+            if (is_array($decoded)) {
+                /** @var array $restResult */
+                $restResult = $this->getDoctrine()->getRepository('AppBundle:user')->findBy(['id' => $decoded['userId']]);
+
+                if ($restResult === null) {
+                    return new View("There are no konto exist", Response::HTTP_NOT_FOUND);
+                }
+
+                $result = [];
+
+                /** @var user $user */
+                foreach ($restResult as $user) {
+                    /** @var konto $konto */
+                    foreach ($user->getKontos() as $konto) {
+                        $result[] = [
+                            'id' => $konto->getId(),
+                            'guthaben' => $konto->getGuthaben(),
+                            'beschreibung' => $konto->getBeschreibung(),
+                            'iban' => $konto->getIban(),
+                            'bic' => $konto->getBic(),
+                            'inhaber' => $konto->getInhaber(),
+                            'kreditkarte' => $konto->getKreditkarte(),
+                            'gueltigBis' => $konto->getGueltigBis()->format('m/Y'),
                             'aktiv' => $konto->getAktiv()
                         ];
                     }
